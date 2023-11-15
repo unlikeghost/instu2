b65bbb3927aa6656b8618a813177d06b111a7215
Luis <luis_fernandez99@hotmail.com>
2023-11-15 11:25:58 -0600

    :art: Estilos agregadis

--- u3/web/main.js
+++ u3/web/main.js
@@ -1,47 +1,55 @@
-async function get_data(){
+async function get_data() {
   let data = await eel.get_data()();
 
   let len = data.length;
 
   let trace1 = {
+    bgcolor: "transparent",
     x: Array.from(Array(len).keys()),
     y: data,
     type: 'scatter',
-    line: {color: "rgb(0,0,0)",
-           width: 3,
-           dtick: 0.25,
-          },
+    line: {
+      color: "rgb(199, 248, 228)",
+      width: 3,
+      dtick: 0.25,
+    },
+
   };
 
   let layout = {
-    plot_bgcolor: "rgb(230,188,197)", 
+    plot_bgcolor: "transparent",
+    paper_bgcolor: "transparent",
+    font: {
+      color: "white",
+      family: "lato, sans-serif",
+    },
     xaxis: {
-      gridcolor: "rgb(208,160,172)",
+      gridcolor: "rgb(0,155,158)",
       autotick: false,
       ticks: 'outside',
       dtick: 10,
-      ticklen: 8,
+      ticklen: 8, 
       title: {
         text: 'Time (ms)',
         font: {
-          family: 'Courier New, monospace',
+          family: "lato, sans-serif",
           size: 18,
-          color: '#7f7f7f'
+          color: '#fff'
         }
       },
 
-    }, 
+    },
     yaxis: {
-      gridcolor: "rgb(208,160,172)",
+      gridcolor: "rgb(0,155,158)",
       autotick: true,
       dtick: 100,
       ticklen: 8,
       title: {
         text: 'Voltage (mV)',
         font: {
-          family: 'Courier New, monospace',
+          family: "lato, sans-serif",
           size: 18,
-          color: '#7f7f7f'
+          color: '#fff'
         },
       },
 
